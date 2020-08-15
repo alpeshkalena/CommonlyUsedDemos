5498b1bbdb4c3dfebb1b6e4ca8830e2325c55a2e
alpeshkalena <alpeshkalena@outlook.com>
2020-08-15 23:25:14 +0530

    Initial commit- Remote Validation For Email

---
+++ CommonlyUsedDemoProjects/RemoteServerValidation/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RemoteServerValidation.Models;
+
+namespace RemoteServerValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new RemoteValidation());
+        }
+
+        [HttpPost]
+        public IActionResult Index(RemoteValidation data)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(data);
+            }
+            return View();
+        }
+
+        //Here if value are match then we have to return false instead of true
+        //that is the noticed by Alpesh and tested
+        [HttpPost]
+        public IActionResult CheckDuplicateEmail(string Email)
+       {
+            var isEmailExist = Email == "xyz123@gmail.com" ? false : true;
+            return Json(isEmailExist);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CommonlyUsedDemoProjects/RemoteServerValidation/Models/RemoteValidation.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RemoteServerValidation.Models
+{
+    public class RemoteValidation
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [Remote("CheckDuplicateEmail", "Home",HttpMethod ="POST",ErrorMessage ="Email Already Exists!!")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "First is required")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last is required")]
+        public string LastName { get; set; }
+    }
+}
